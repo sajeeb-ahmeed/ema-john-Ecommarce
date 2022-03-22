805a58e11e11259edccef5de2b0321064b1e8b0c
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-03-22 16:00:43 +0600

    react-bootstrap added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Shop.css'
+const Shop = () => {
+    return (
+        <div className='shop'>
+            <div className="shop-products">
+                <h3>this shop container</h3>
+            </div>
+            <div className="order-details">
+                <h4>this is order details</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
