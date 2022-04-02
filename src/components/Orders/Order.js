34a9ce14c8652feb752d7a3c146e5e594ec34159
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-04-02 16:08:00 +0600

    order summary component added

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import useCart from '../../Hooks/useProducts/useCart';
 import useProducts from '../../Hooks/useProducts/useProducts';
+import Cart from '../Cart/Cart';
+import '../Cart/Cart.css'
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Order = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products)
     return (
         <div>
-            <h1>This is order Component {products.length}</h1>
-            <p>Cart has {cart.length}</p>
+            <div className="container-fluid">
+                <div className="container mt-5">
+                    <div className="row">
+                        <div className="col-md-7">
+
+                            {
+                                cart.map(product => <ReviewItem key={product.id} product={product}></ReviewItem>)
+                            }
+                        </div>
+                        <div className="col-md-4   bg-info">
+                            <Cart key={cart.id} cart={cart}></Cart>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDeleteLeft, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+const ReviewItem = (props) => {
+    const { name, img, price, shipping, quantity } = props.product
+    return (
+        <div>
+            <div className='container '>
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-between py-2 mb-3 bg-info">
+                        <div className='d-flex mr-1 justify-content-center align-items-center'>
+                            <div className=''>
+                                <img className='me-4 rounded ' src={img} style={{ width: '80px', height: '80px' }} alt="" />
+                            </div>
+                            <div>
+                                <h5> {name}</h5>
+                                <p>Price : ${price}</p>
+                                <p>Shipping:{shipping}</p>
+                                <p> Quantity: {quantity}</p>
+                            </div>
+
+                        </div>
+                        <div>
+                            <FontAwesomeIcon className='p-3 cursor-pointer btn-outline-danger' icon={faTrashAlt} />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default ReviewItem;
